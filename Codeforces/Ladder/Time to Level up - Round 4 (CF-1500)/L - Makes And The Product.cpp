4f1795b56783e08f72ac8ee2e13dfc29bd9b8e31
Crazy_dreamer <38868670+s-m-nahid-hasan-ruet-ece@users.noreply.github.com>
2022-11-02 06:07:17 +0600

    new update

---
+++ Codeforces/Ladder/Time to Level up - Round 4 (CF-1500)/L - Makes And The Product.cpp
@@ -0,0 +1,107 @@
+
+/*
+     Author's Handle : Crazy_Dreamer
+     Author's Name   : S M Nahid Hasan                
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll               long long
+#define dl               double
+#define forn(i,a,n)      for (ll i=a;i<n;i++)
+#define per(i,a,n)       for (ll i=n-1;i>=a;i--)
+#define pb               push_back
+#define mp               make_pair
+#define all(x)           (x).begin(),(x).end()
+#define fi               first
+#define se               second
+#define bug              cout<<"bug --  "<<bug1++<<el;
+#define SZ(x)            ((ll)(x).size())
+#define el               endl
+#define pi               acos(-1.0)
+#define fio              ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define tst              cin>>t; while(t--)
+#define sz               200050
+
+
+
+typedef    pair<ll,ll>pll;
+typedef    pair<ll,pll>pl;
+typedef    unordered_map<ll,ll>mll;
+typedef    vector<ll>vl;
+typedef    vector<pll>vll;
+typedef    vector<pl>vlp;
+typedef    set<ll>sl;
+typedef    multiset<ll>msl;
+typedef    priority_queue<ll>prl;
+
+const ll mod=1000000007;
+
+int main()
+{
+
+    fio
+
+    ll t,i,j,n,m,k,a,b,c=0,tc=1,bug1=1;
+
+    //tst
+    {
+    	cin>>n;
+    	mll mp;
+    	ll ar[n+5];
+        vl vc;
+    	
+    	forn(i,0,n)
+    	 {
+    	 	cin>>ar[i];
+    	 	mp[ar[i]]++;
+            if(mp[ar[i]]==1)
+                vc.pb(ar[i]);
+    	 }
+    	 
+    	sort(vc.begin(),vc.end());
+    	ll ans = 1;
+    	
+    	if(mp[vc[0]]>2)
+    	{
+    		ll x = 3;
+    		while(x>0)
+    		{
+    			ans*=(mp[vc[0]]--);
+    			x--;
+    		}
+    		ans/=6;
+    	}
+    	else
+    	{
+    		if(mp[vc[0]]+mp[vc[1]]>=3)
+    		{
+    			if(mp[vc[0]]==1)
+    			{
+    				ans=mp[vc[1]]*(mp[vc[1]]-1);
+    				ans/=2;
+    			}
+    			else
+    			{
+    				ans=mp[vc[1]];
+    			}
+    		}
+    		else
+    		{
+    			ans=mp[vc[2]];
+    		}
+    	}
+    	
+    	
+    	cout<<ans<<el;
+
+
+    }
+
+
+    return 0;
+}
+
+
+
